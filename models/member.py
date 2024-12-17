3d3826015256fecbfbc97d47864515dcfffba631
Rere Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2024-12-17 20:45:25 +0700

    refactor: default member pydantic

--- models/member.py
+++ models/member.py
@@ -6,6 +6,7 @@ class Member(BaseModel):
     id: int
     timestamp_daftar: datetime = Field(default_factory=datetime.now)
     email: str
+    password: str
     nama_lengkap: constr(max_length=255)
     provinsi: constr(max_length=255)
     kota: constr(max_length=255)
@@ -13,11 +14,18 @@ class Member(BaseModel):
     kelurahan: Optional[constr(max_length=255)] = None
     alamat_lengkap: str
     nomor_whatsapp: constr(max_length=20)
-    jenis_kelamin: constr(max_length=1, regex='^(L|P)$')
+    jenis_kelamin: str = Field(..., max_length=1, pattern='^(L|P)$')
     tanggal_lahir: date
-    pendidikan_terakhir: conint(ge=0, le=9)
+    pendidikan_terakhir: int = Field(..., ge=0, le=9)
     profesi: Optional[constr(max_length=255)] = None
     referensi: Optional[str] = None
     harapan: Optional[str] = None
     is_verified: bool = False
     is_active: bool = False
+    is_admin: bool = False
+
+class MemberResponse(BaseModel):
+    id: int
+    nama_lengkap: str
+    email: str
+    nomor_whatsapp: str
