e16731f40e9bc0602e794ac519234c856c4ad754
Rere Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2024-12-22 22:35:15 +0700

    feat: support reset password

--- routes/member.py
+++ routes/member.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends, Query
+from sqlalchemy import select, update
 from databases import Database
 from services.member.all import fetch_all_members_paginated, search_members
 from models.member import Member, MemberResponse, PaginatedMembersResponse, MemberCreate
@@ -6,8 +7,10 @@ from services.member.specific import fetch_member_by_id
 from services.member.card import get_current_user
 from services.member.add import add_new_member
 from config.database import get_database,members_table
+from passlib.context import CryptContext
 
 member_router = APIRouter()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @member_router.get("/card")
 async def read_users_card(current_user: dict = Depends(get_current_user)):
@@ -73,3 +76,28 @@ async def add_member(member_data: MemberCreate, database: Database = Depends(get
         raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+@member_router.put("/reset-password/{id}")
+async def reset_password(id: int, current_user: dict = Depends(get_current_user), database: Database = Depends(get_database)):
+    if not current_user["is_admin"]:
+        raise HTTPException(status_code=403, detail="Not authorized to reset password")
+    try:
+        query = select(members_table.c.nomor_whatsapp).where(members_table.c.id == id)
+
+        member = await database.fetch_one(query)
+
+        if not member:
+            raise HTTPException(status_code=404, detail="Member not found")
+
+        hashed_password = pwd_context.hash(member["nomor_whatsapp"])
+
+        update_query = (
+            update(members_table)
+            .where(members_table.c.id == id)
+            .values(password=hashed_password)
+        )
+        await database.execute(update_query)
+        return {"message": "Password reset to default successfully"}
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail=f"Failed to reset password: {str(e)}")
