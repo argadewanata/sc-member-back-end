9f2682eb5564dc927f7454c3ecbb5523706d8455
Rere Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2024-12-22 19:21:24 +0700

    feat: add new member for admin

--- routes/member.py
+++ routes/member.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends, Query
 from databases import Database
-from models.member import Member, MemberResponse, PaginatedMembersResponse
 from services.member.all import fetch_all_members_paginated, search_members
+from models.member import Member, MemberResponse, PaginatedMembersResponse, MemberCreate
 from services.member.specific import fetch_member_by_id
 from services.member.card import get_current_user
+from services.member.add import add_new_member
 from config.database import get_database
 
 member_router = APIRouter()
@@ -48,3 +49,13 @@ async def get_member_by_id(id: int, database: Database = Depends(get_database)):
         raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@member_router.post("/add")
+async def add_member(member_data: MemberCreate, database: Database = Depends(get_database)):
+    try:
+        await add_new_member(member_data, database)
+        return {"message": "Member added successfully"}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
