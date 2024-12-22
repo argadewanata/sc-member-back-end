9c91ceb2ab85241e962a4eb487f51a3b7ee2a6de
Rere Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2024-12-22 18:48:37 +0700

    feat: search member by id

--- services/member/specific.py
+++ services/member/specific.py
@@ -1,16 +1,11 @@
-import logging
 from sqlalchemy import select
 from databases import Database
-from fastapi import HTTPException
 from models.member import Member
-from config.database import members_table, database
+from config.database import members_table
 
 async def fetch_member_by_id(id: int, database: Database):
-    logging.info(f"Fetching member with ID: {id}")
     query = select(members_table).where(members_table.c.id == id)
-    row = await database.fetch_one(query)
-    if row is None:
-        logging.warning(f"Member with ID {id} not found")
+    member = await database.fetch_one(query)
+    if not member:
         raise HTTPException(status_code=404, detail="Member not found")
-    logging.info(f"Member with ID {id} fetched successfully")
-    return Member(**row)
+    return Member(**member)
